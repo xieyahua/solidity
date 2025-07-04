8522cfa947538d642d22628f6321b335ac08327d
xieyahua <138201509+xieyahua@users.noreply.github.com>
2025-07-04 09:15:06 +0800

    Update task1.sol

--- task1.sol
+++ task1.sol
@@ -33,3 +33,23 @@ contract Voting {
         delete _candidates;
     }
 }
+
+
+
+------------------------------------------2.反转字符串 (Reverse String)----------------------------------------------------------------
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract StringReverser {
+    function reverse(string memory _str) public pure returns (string memory) {
+        bytes memory strBytes = bytes(_str);
+        bytes memory reversed = new bytes(strBytes.length);
+        
+        for(uint i = 0; i < strBytes.length; i++) {
+            reversed[i] = strBytes[strBytes.length - i - 1];
+        }
+        
+        return string(reversed);
+    }
+}
+
