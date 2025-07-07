863f0b7d6eefb894f462577fc0e46cb56fac5aaa
xieyahua <138201509+xieyahua@users.noreply.github.com>
2025-07-07 09:15:25 +0800

    Update task1.sol

--- task1.sol
+++ task1.sol
@@ -163,3 +163,28 @@ contract MergeSortedArrays {
     }
 }
 
+------------------------------6.二分查找 (Binary Search)---------------------------------------
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BinarySearch {
+    function search(uint[] memory arr, uint target) public pure returns (int) {
+        uint left = 0;
+        uint right = arr.length - 1;
+        
+        while (left <= right) {
+            uint mid = left + (right - left) / 2;
+            
+            if (arr[mid] == target) {
+                return int(mid);
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+
