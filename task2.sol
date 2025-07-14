3d229c8d885896a372339adb10bd1133ddd6e587
xieyahua <138201509+xieyahua@users.noreply.github.com>
2025-07-14 09:56:43 +0800

    Update task2.sol

--- task2.sol
+++ task2.sol
@@ -1,1 +1,94 @@
 
+---------------------------------------------------作业 1：ERC20 代币----------------------------------------------------------------------------
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+    address public owner;
+    
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Mint(address indexed to, uint256 amount);
+    
+    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) {
+        name = _name;
+        symbol = _symbol;
+        owner = msg.sender;
+        _mint(msg.sender, _initialSupply * 10**decimals);
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this");
+        _;
+    }
+    
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+    
+    function transfer(address to, uint256 amount) public returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+    
+    function approve(address spender, uint256 amount) public returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+    
+    function allowance(address _owner, address spender) public view returns (uint256) {
+        return _allowances[_owner][spender];
+    }
+    
+    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
+        _spendAllowance(from, msg.sender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+    
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    
+    function _transfer(address from, address to, uint256 amount) internal {
+        require(from != address(0), "Transfer from zero address");
+        require(to != address(0), "Transfer to zero address");
+        require(_balances[from] >= amount, "Insufficient balance");
+        
+        _balances[from] -= amount;
+        _balances[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+    
+    function _approve(address _owner, address spender, uint256 amount) internal {
+        require(_owner != address(0), "Approve from zero address");
+        require(spender != address(0), "Approve to zero address");
+        
+        _allowances[_owner][spender] = amount;
+        emit Approval(_owner, spender, amount);
+    }
+    
+    function _spendAllowance(address _owner, address spender, uint256 amount) internal {
+        uint256 currentAllowance = allowance(_owner, spender);
+        if (currentAllowance != type(uint256).max) {
+            require(currentAllowance >= amount, "Insufficient allowance");
+            _approve(_owner, spender, currentAllowance - amount);
+        }
+    }
+    
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "Mint to zero address");
+        
+        totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+        emit Mint(account, amount);
+    }
+}
